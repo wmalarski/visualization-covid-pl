623e2636a8b06b00b8888d0dd365695f59608f2f
wojciech <w.malarski@gmail.com>
2020-11-14 20:43:01 +0100

    eslint + typescript setup + material

---
+++ pages/_app.tsx
@@ -0,0 +1,35 @@
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import type { AppProps /*, AppContext */ } from "next/app";
+import Head from "next/head";
+import React, { useEffect } from "react";
+import theme from "../themes/theme";
+
+export default function MyApp(props: AppProps): JSX.Element {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles?.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Covid Visualization</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from "next/head";
+import React from "react";
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <div className="container">
       <Head>
@@ -54,7 +55,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +206,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
